0641f901f62a17c37492fd2f68692796afe3bd0b
soulboy <soulllboy1@gmail.com>
2021-08-16 19:36:47 +0500

    Base style without scripts

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-var myFunc = function() {
-  
-};
+var myFunc = function(id) {
+  document.getElementsById(id).style.color=red;
+}
+buyEnergy = function() {
+  var i = document.getElementsById("energyCounter");
+  var j = document.getElementsByClassName("frontBar");
+  i.innerHTML = parseInt(i.innerHTML) - 20;
+  j.style.width = i.innerHTML+"%";
+}
